bab0bf10f719e2b9a76616d5e2a8da29f4e49d1a
manavnotop <manuagarwal1443@gmail.com>
2025-08-02 15:47:29 +0530

    Add: Feedforward network

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from toy-llama32!")
+import torch
+import torch.nn as nn
 
+class FeedForward(nn.Module):
+  def __init__(self, cfg):
+    super().__init__()
+    self.fc1 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"] ,bias=False)
+    self.fc2 = nn.Linear(cfg["emb_dim"], cfg["hidden_dim"], dtype=cfg["dtype"] ,bias=False)
+    self.fc3 = nn.Linear(cfg["hidden_dim"], cfg["emb_dim"], dtype=cfg["dtype"] ,bias=False)
 
-if __name__ == "__main__":
-    main()
+  def forward(self, x):
+    x_fc1 = self.fc1(x)
+    x_fc2 = self.fc2(x)
+    x = nn.functional.silu(x_fc1) * x_fc2
+    return self.fc3(x)
+
