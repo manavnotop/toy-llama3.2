62227202d8945e05660fee5c55a63b25e190775f
manavnotop <manuagarwal1443@gmail.com>
2025-08-02 16:07:30 +0530

    Add: RMSNorm

--- main.py
+++ main.py
@@ -10,7 +10,28 @@ class FeedForward(nn.Module):
 
   def forward(self, x):
     x_fc1 = self.fc1(x)
+    x = nn.RMSNorm()
     x_fc2 = self.fc2(x)
     x = nn.functional.silu(x_fc1) * x_fc2
     return self.fc3(x)
+
+
+class RMSNorm(nn.Module):
+  def __init__(self, emb_dim, eps=1e-6):
+    super().__init__()
+    self.emb_dim = emb_dim
+    self.eps = eps
+    #initialise trainable parameters for RMSNorm
+    self.scale = nn.Parameter(torch.ones(emb_dim))
+  
+  def forward(self, x):
+    input_type = x.dtype
+    #temp upcast for numerical stability, as square and roots can be unstable in float16
+    x = x.to(torch.float32)
+
+    mean_square = x.pow(2).mean(dim=-1, keepdim=True)
+    norm_x = x * torch.rsqrt(mean_square + self.eps)
+    norm_x = norm_x * self.scale
+
+    return norm_x.to(input_type)
 
